d9e23b66acdfaca0073c0e9fba00e0771a1dd484
BinalPatel29 <binaldpatel29@gmail.com>
2026-08-07 18:38:09 +0530

    correct logOut path

--- my-app/src/components/chat/ChatHeader.jsx
+++ my-app/src/components/chat/ChatHeader.jsx
@@ -1,14 +1,14 @@
 import styles from '../../pages/ChatPage.module.css';
 
-function ChatHeader({ roomName, onLogout }){
-    return (
-        <header className= { styles.ChatHeader }>
-            <h1>{roomName}</h1>
-            <button onClick={ onLogout } className= { styles.addLogout }>
-                LOG OUT
-            </button>
-        </header>
-    );
+function ChatHeader({ roomName, onLogout }) {
+  return (
+    <header className={styles.ChatHeader}>
+      <h1>{roomName}</h1>
+      <button className={styles.btnLogout} onClick={onLogout}>
+        LOG OUT
+      </button>
+    </header>
+  );
 }
 
 export default ChatHeader;
